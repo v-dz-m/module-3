a5592c4210faf8ce796be175abcb9cb9e4ea6c1a
Vadzim Hanchuk <vadimganchuk0201@gmail.com>
2024-01-11 16:55:26 +0300

    Add AI check for best lines
    If AI has taken 2 cells from best line and 3rd cell is empty, AI takes it
    Also some naming fixes and add new functions

--- task_4_tic_tac_toe.cpp
+++ task_4_tic_tac_toe.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 const int FIRST_AI_MOVES[] = { 4, 2, 6 };
 const int CELL_AMOUNT = 9;
-const int ROW_LENGTH = 3;
+const int LINE_LENGTH = 3;
+
+const int WIN_LINES = 8;
+const int WIN_LINE_INDEXES[WIN_LINES][LINE_LENGTH] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6} };
+const int BEST_LINES = 24;
+const int BEST_LINE_INDEXES[BEST_LINES][LINE_LENGTH] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}, {0, 2, 1}, {3, 5, 4}, {6, 8, 7}, {0, 6, 3}, {1, 7, 4}, {2, 8, 5}, {0, 8, 4}, {2, 6, 4}, {1, 2, 0}, {4, 5, 3}, {7, 8, 6}, {3, 6, 0}, {4, 7, 1}, {5, 8, 2}, {4, 8, 0}, {4, 6, 2} };
 
 int gameCells[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 int freeCells = CELL_AMOUNT;
@@ -19,7 +24,14 @@ void printGameRow(int rowNumber);
 void userMove();
 void aiMove();
 bool isWin(int current);
+void printWin(int current);
 bool isDraw();
+int firstAIMove();
+int secondAIMove();
+int bestAIMove();
+int getBestLineCell(int current);
+int repelAIMove();
+int randomAIMove();
 
 int main()
 {
@@ -65,7 +77,7 @@ void printField()
 {
     cout << endl << "—————————————    —————————————";
     cout << endl;
-    for (int i = 0; i < ROW_LENGTH; i++) {
+    for (int i = 0; i < LINE_LENGTH; i++) {
         printDigitRow(i);
         cout << "   ";
         printGameRow(i);
@@ -78,8 +90,8 @@ void printField()
 void printGameRow(int rowNumber)
 {
     cout << "| ";
-    for (int i = 0; i < ROW_LENGTH; i++) {
-        int currentCell = gameCells[rowNumber * ROW_LENGTH + i];
+    for (int i = 0; i < LINE_LENGTH; i++) {
+        int currentCell = gameCells[rowNumber * LINE_LENGTH + i];
         if (currentCell == -1) {
             cout << (isUserFirst ? "X" : "O");
         }
@@ -96,8 +108,8 @@ void printGameRow(int rowNumber)
 void printDigitRow(int rowNumber)
 {
     cout << "| ";
-    for (int i = 0; i < ROW_LENGTH; i++) {
-        cout << (rowNumber * ROW_LENGTH + i + 1) << " | ";
+    for (int i = 0; i < LINE_LENGTH; i++) {
+        cout << (rowNumber * LINE_LENGTH + i + 1) << " | ";
     }
 }
 
@@ -131,66 +143,60 @@ void userMove()
 
 void aiMove()
 {
+    int move = -1;
     printField();
-    if (freeCells < CELL_AMOUNT) {
-        // after-move if user goes first
-        if (freeCells == CELL_AMOUNT - 1) {
-            if (gameCells[4] == 0) {
-                gameCells[4] = 1;               // if the middle cell is empty take it
-            }
-            else {
-                int randomNumber = (rand() % 4) * 2;
-                gameCells[randomNumber] = 1;    // otherwise take a random corner cell
-            }
-        }
-        else {
-            while (true) {
-                int randomNumber = rand() % CELL_AMOUNT;
-                if (gameCells[randomNumber] == 0) {
-                    cout << endl << "AI move: " << (randomNumber + 1) << endl;
-                    gameCells[randomNumber] = 1;
-                    break;
-                }
-            }
-        }
-        freeCells--;
-        if (isWin(1)) {
-            return;
-        }
-        if (isDraw()) {
-            return;
-        }
+    if (freeCells == CELL_AMOUNT) {
+        move = firstAIMove();
+    }
+    else if (freeCells == CELL_AMOUNT - 1) {
+        move = secondAIMove();
     }
     else {
-        int move = FIRST_AI_MOVES[aiFirstCounter];
-        aiFirstCounter = (aiFirstCounter + 1) % ROW_LENGTH;
-        cout << endl << "AI move: " << (move + 1) << endl;
-        gameCells[move] = 1;
+        move = bestAIMove();
     }
+    gameCells[move] = 1;
 
+    cout << endl << "AI move: " << (move + 1) << endl;
+
+    freeCells--;
+    if (isWin(1)) {
+        return;
+    }
+    if (isDraw()) {
+        return;
+    }
     userMove();
 }
 
 bool isWin(int current)
 {
-    if ((gameCells[0] == current && gameCells[1] == current && gameCells[2] == current) ||
-        (gameCells[3] == current && gameCells[4] == current && gameCells[5] == current) ||
-        (gameCells[6] == current && gameCells[7] == current && gameCells[8] == current) ||
-        (gameCells[0] == current && gameCells[3] == current && gameCells[6] == current) ||
-        (gameCells[1] == current && gameCells[4] == current && gameCells[7] == current) ||
-        (gameCells[2] == current && gameCells[5] == current && gameCells[8] == current) ||
-        (gameCells[0] == current && gameCells[4] == current && gameCells[8] == current) ||
-        (gameCells[2] == current && gameCells[4] == current && gameCells[6] == current)) {
-        printField();
-        if (current == -1) {
-            cout << endl << "User wins!" << endl;
+    for (int i = 0; i < WIN_LINES; i++) {
+        // check all win line cells taken by current
+        bool isWinLine = true;
+        for (int j = 0; j < LINE_LENGTH; j++) {
+            if (gameCells[WIN_LINE_INDEXES[i][j]] != current) {
+                isWinLine = false;
+                break;
+            }
         }
-        else {
-            cout << endl << "AI wins!" << endl;
+        if (isWinLine) {
+            printWin(current);
+            return true;
         }
-        return true;
     }
+
     return false;
+}
+
+void printWin(int current)
+{
+    printField();
+    if (current == -1) {
+        cout << endl << "User wins!" << endl;
+    }
+    else {
+        cout << endl << "AI wins!" << endl;
+    }
 }
 
 bool isDraw()
@@ -200,5 +206,59 @@ bool isDraw()
     }
     printField();
     cout << endl << "This is a draw!" << endl;
+
     return true;
+}
+
+int firstAIMove()
+{
+    int move = FIRST_AI_MOVES[aiFirstCounter];
+    aiFirstCounter = (aiFirstCounter + 1) % LINE_LENGTH;
+
+    return move;
+}
+
+int secondAIMove()
+{
+    if (gameCells[4] == 0) {
+        return 4;               // if the middle cell is empty take it
+    }
+
+    return (rand() % 4) * 2;    // otherwise take a random corner cell
+}
+
+int bestAIMove()
+{
+    int bestCell = getBestLineCell(1);
+    if (bestCell > -1) {
+        return bestCell;
+    }
+
+    return repelAIMove();
+}
+
+int getBestLineCell(int current)
+{
+    for (int i = 0; i < BEST_LINES; i++) {
+        if (gameCells[BEST_LINE_INDEXES[i][0]] == current && gameCells[BEST_LINE_INDEXES[i][1]] == current && gameCells[BEST_LINE_INDEXES[i][2]] == 0) {
+            return BEST_LINE_INDEXES[i][2];
+        }
+    }
+
+    return -1;
+}
+
+int repelAIMove()
+{
+    return randomAIMove();
+}
+
+int randomAIMove()
+{
+    while (true) {
+        int randomNumber = rand() % CELL_AMOUNT;
+        if (gameCells[randomNumber] == 0) {
+            return randomNumber;
+        }
+    }
 }
